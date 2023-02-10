aedf6cad71c72f61db6479d75eab74c30a09b6cb
SabiMantock <sabimantock@gmail.com>
2023-02-10 13:03:56 +0000

    feat/redux_in_react: complete main features: - Connected to store - Added event handler that adds and removes books from the array

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,29 +1,49 @@
 import React from 'react';
+
+import { useDispatch } from 'react-redux';
 import Button from '../Button/Button';
 import Input from '../Input/Input';
+import { addBookItem } from '../../redux/books/books';
 
-const Form = () => (
-  <form>
-    <Input
-      type="text"
-      placeholder="title"
-      value="title"
-      name="title"
-      onChange={(e) => console.log(e.target.value)}
-    />
-    <Input
-      type="text"
-      placeholder="author"
-      value="author"
-      name="author"
-      onChange={(e) => console.log(e.target.value)}
-    />
-    <Button
-      title="Add Book"
-      className="btn"
-      onClick={() => console.log('clicked')}
-    />
-  </form>
-);
+const Form = () => {
+  const dispatch = useDispatch();
+
+  const handleAddBook = (e) => {
+    e.preventDefault();
+    dispatch(addBookItem(
+      {
+        item_id: 'item4',
+        title: 'Anna Karenina',
+        author: 'Leo Tolstoy',
+        category: 'Fiction',
+      },
+    ));
+  };
+
+  return (
+    <form onSubmit={handleAddBook}>
+      <Input
+        type="text"
+        placeholder="title"
+        value="title"
+        name="title"
+        onChange={(e) => console.log(e.target.value)}
+      />
+      <Input
+        type="text"
+        placeholder="author"
+        value="author"
+        name="author"
+        onChange={(e) => console.log(e.target.value)}
+      />
+      <Button
+        defaultProps
+        title="Add Book"
+        className="btn"
+        onClick={() => 'clicked'}
+      />
+    </form>
+  );
+};
 
 export default Form;
